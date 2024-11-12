20e8c2fc9927960fbfea0de38b6d72a5455aa48f
Jatin24062005 <Jatin04072005@gmail.com>
2024-11-12 20:21:45 +0530

    Initial commit of backend code

--- Backend/utillis/datauri.js
+++ Backend/utillis/datauri.js
@@ -1,13 +1,16 @@
 // datauri.js
-import DatauriParser from "datauri/parser";
+import DatauriParser from "datauri/parser.js";
+import { get } from "http";
 import path from "path";
 
 const parser = new DatauriParser();
 
-export const getDataUri = (file) => {
+ const getDataUri = (file) => {
     if (!file || !file.originalname || !file.buffer) {
         throw new Error("Invalid file input"); // Improved error message
     }
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName, file.buffer).content;
 };
+
+export default getDataUri;
