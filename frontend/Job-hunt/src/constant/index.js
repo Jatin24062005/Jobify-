7bf495c8571a030b6405d934947b327a3222baf0
JATIN <Jatin04072005@gmail.com>
2024-11-28 23:21:55 +0530

    Update index.js

--- frontend/Job-hunt/src/constant/index.js
+++ frontend/Job-hunt/src/constant/index.js
@@ -4,7 +4,7 @@ export const navLists = [
     { name: "Job", path: "/jobs" },
     { name: "Browse", path: "/browse" }
 ];
-export const USER_API_END_POINT="https://jobify-jatin-backend.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://jobify-jatin-backend.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobify-jatin-backend.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://jobify-jatin-backend.onrender.com/api/v1/company";
+export const USER_API_END_POINT="https://jobify-backend-fd9m.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://jobify-backend-fd9m.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobify-backend-fd9m.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://jobify-backend-fd9m.onrender.com/api/v1/company";
