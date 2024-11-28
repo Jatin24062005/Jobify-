913d2cf4bc22c7d72d621b673fd889a8d3c43214
JATIN <Jatin04072005@gmail.com>
2024-11-28 23:26:25 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobifyjatin.vercel.app/',
+    origin:'https://jobifyjatin.vercel.app',
     credentials:true
 }
 
