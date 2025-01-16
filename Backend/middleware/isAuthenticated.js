9d82bfc393c795e7d3aef826c235b6e4e581abd1
JATIN <Jatin04072005@gmail.com>
2025-01-16 18:07:25 +0530

    fixed cookie

--- Backend/middleware/isAuthenticated.js
+++ Backend/middleware/isAuthenticated.js
@@ -2,7 +2,7 @@ import  jwt  from "jsonwebtoken";
 
   const isAuthenticated = async (req,res,next)=>{
     try {
-        const token = req.cookies.token;
+        const token = req.cookie.token;
         if(!token){
             return res.status(400).json({
                 message:"User not Authenticated",
